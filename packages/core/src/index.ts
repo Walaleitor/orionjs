fb78e46dac45d2c45c096009bd4d862310a7effa
walaleitor <sebanicosp@gmail.com>
2022-03-24 12:41:49 -0300

    created generate resource command

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import {Command} from 'commander'
+import {Command, Argument} from 'commander'
 import start from './start'
 import build from './build'
 import colors from 'colors/safe'
@@ -7,6 +7,7 @@ import test from './test'
 import './handleErrors'
 import version from './version'
 import 'dotenv/config'
+import generate from './generate'
 
 const program = new Command()
 
@@ -34,6 +35,29 @@ program
   .description('Compiles an Orionjs app and exports it to a simple nodejs app')
   .option('-o, --output [output]', 'Output directory')
   .action(run(build))
+
+program
+  .command('generate')
+  .alias('g')
+  .addArgument(
+    new Argument('<resources>', 'resource for orionsjs').choices([
+      //infra
+      'resolver',
+      'echoes',
+      'jobs',
+      'http',
+      //application
+      'service',
+      //domain
+      'collections',
+      'repo',
+      'model'
+    ])
+  )
+  .option('-t, --type [type]', 'description')
+  .action((params, option) => {
+    generate('params', 'option')
+  })
 
 program.version(version, '-v --version')
 
